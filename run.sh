7197c108fc9c5d2922e0c397ad245eac573751c1
Sethjsa <seth@manx.net>
2024-05-08 10:08:24 +0200

    update run

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-python panlex_bilingual_extract.py --source_language=spa --target_language=spa --output_directory=data/lexicons
+!#/bin/bash
 
+python /home/saycock/personal/data/panlex/Panlex-Lexicon-Extractor/panlex_bilingual_extract.py \
+    --source_language $1 --target_language $2 \
+    --output_directory /home/saycock/personal/data/panlex/updated/data/lexicons \
+    --panlex_dir /home/saycock/personal/data/panlex/updated/data/ \
+    --sql_database /home/saycock/personal/data/panlex/updated/data/panlex.db
+
+# usage:
+# bash run.sh spa eng
